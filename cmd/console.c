xypron/fml13v01-uboot

commit cf7ee6d2383536e7febc17b1a19f24cae57a407a

Fix some issues and change the logo background color. 1. Fix the issue where GRUB cannot return to the U-Boot command line after exiting. 2. Fix the support for vidconsole. 3. Change the logo to a black background.
Signed-off-by: yang.wang <yang.wang@deepcomputing.io>

cmd/console.c
@@ -43,6 +43,21 @@ static int do_coninfo(struct cmd_tbl *cmd, int flag, int argc,
 	return 0;
 }
 
+static int do_cls(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = uclass_get_device(UCLASS_VIDEO, 0, &dev);
+	if (ret)
+		return ret;
+
+	video_clear(dev);
+	env_set("stdout", "serial,vidconsole");
+
+	printf("\x1b[2J\x1b[H");
+	return 0;
+}
 
 /***************************************************/
 
@@ -50,4 +65,10 @@ U_BOOT_CMD(
 	coninfo,	3,	1,	do_coninfo,
 	"print console devices and information",
 	""
+);
+
+U_BOOT_CMD(
+    cls, 1, 1, do_cls,
+    "Clear screen",
+    ""
 );

configs/starfive_visionfive2_defconfig
@@ -30,7 +30,7 @@ CONFIG_SPI_BOOT=y
 CONFIG_USE_BOOTARGS=y
 CONFIG_BOOTARGS="console=tty1 console=ttyS0,115200  debug rootwait  earlycon=sbi"
 CONFIG_USE_BOOTCOMMAND=y
-CONFIG_BOOTCOMMAND="sleep 2;usb start;run efi_boot_env;run sdk_boot_env; run distro_boot_env"
+CONFIG_BOOTCOMMAND="sleep 2;usb start;cls;run efi_boot_env;run sdk_boot_env; run distro_boot_env"
 CONFIG_USE_PREBOOT=y
 CONFIG_PREBOOT="run chipa_set_uboot;"
 CONFIG_DEFAULT_FDT_FILE="/boot/dtbs/starfive/jh7110-deepcomputing-fml13v01.dtb"
@@ -168,3 +168,4 @@ CONFIG_DISPLAY_STARFIVE_HDMI=y
 CONFIG_DISPLAY_STARFIVE_MIPI=y
 CONFIG_VIDEO_NW_MIPI_DSI=y
 CONFIG_OF_LIBFDT_OVERLAY=y
+CONFIG_SYS_WHITE_ON_BLACK=y

include/config_distro_bootcmd.h
@@ -469,6 +469,7 @@
 		"echo Scanning ${devtype} "                               \
 				"${devnum}:${distro_bootpart}...; "       \
 		"for prefix in ${boot_prefixes}; do "                     \
+			"cls; "                     \
 			"run scan_dev_for_extlinux; "                     \
 			"run scan_dev_for_scripts; "                      \
 		"done;"                                                   \

commit de166018a25598aa116d97cae7a95dee4f60b13b

cmd: console: add missing includes
Avoid errors

    ../cmd/console.c:51:15: error: implicit declaration of function
    ‘uclass_get_device’ [-Wimplicit-function-declaration]
       51 |         ret = uclass_get_device(UCLASS_VIDEO, 0, &dev);

    ../cmd/console.c:55:9: error: implicit declaration of function ‘video_clear’ [-Wimplicit-function-declaration]
       55 |         video_clear(dev);
          |         ^~~~~~~~~~~

Fixes: cf7ee6d23835 ("Fix some issues and change the logo background color. 1. Fix the issue where GRUB cannot return to the U-Boot command line after exiting. 2. Fix the support for vidconsole. 3. Change the logo to a black background.")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

cmd/console.c
@@ -10,6 +10,8 @@
 #include <common.h>
 #include <command.h>
 #include <stdio_dev.h>
+#include <video.h>
+#include <dm/uclass.h>
 
 extern void _do_coninfo (void);
 static int do_coninfo(struct cmd_tbl *cmd, int flag, int argc,