xypron/fml13v01-uboot

commit ade2e949129c4c9e505e4a141fd8362306a178ca

add switch gpio init.
Signed-off-by: yang.wang <yang.wang@deepcomputing.io>

board/starfive/visionfive2/starfive_visionfive2.c
@@ -413,6 +413,11 @@ static void jh7110_gpio_init(void)
 	/* Enable pwm backlight */
 	SYS_IOMUX_DOEN(22, 0);
 	SYS_IOMUX_DOUT(22, 1);
+
+	/* Switch gpio init */
+	SYS_IOMUX_DOEN(56, 0);
+	SYS_IOMUX_DOUT(56, 0);
+	writel(0, 0x13040200);
 }
 
 int board_init(void)

commit b78beb9e9795f7338b74d49a22d4a9006a299835

board: fix jh7110_gpio_init -Wint-conversion
Avoid error

../board/starfive/visionfive2/starfive_visionfive2.c:
    In function ‘jh7110_gpio_init’:
    ../board/starfive/visionfive2/starfive_visionfive2.c:430:19: error:
    passing argument 2 of ‘writel’ makes pointer from integer without a
    cast [-Wint-conversion]
      430 |         writel(0, 0x13040200);
          |                   ^~~~~~~~~~
          |                   |
          |                   int

Fixes: ade2e949129c ("add switch gpio init.")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

board/starfive/visionfive2/starfive_visionfive2.c
@@ -427,7 +427,7 @@ static void jh7110_gpio_init(void)
 	/* Switch gpio init */
 	SYS_IOMUX_DOEN(56, 0);
 	SYS_IOMUX_DOUT(56, 0);
-	writel(0, 0x13040200);
+	writel(0, (void *)0x13040200);
 }
 
 int board_init(void)